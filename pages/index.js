c40833e133234634b8466132fb9f16503c9da968
Avishek <avishekd653@gmail.com>
2022-04-27 13:58:35 +0530

    pizzaList & pizzaCard has been Done.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Featured from "../components/Featured";
+import PizzaList from "../components/PizzaList";
 // import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
@@ -12,6 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured/>
+      <PizzaList/>
       
     </div>
   );
