88f1d1c5d175314d35be89f1ee08c5c830b82932
Avishek <avishekd653@gmail.com>
2022-04-27 13:26:22 +0530

    Featured done.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Featured from "../components/Featured";
 // import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
@@ -10,7 +11,7 @@ export default function Home() {
         <meta name="description" content="Best Pizza Shop In our Town" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      homepage
+      <Featured/>
       
     </div>
   );
